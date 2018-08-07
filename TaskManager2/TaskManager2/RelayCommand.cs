c300cfd3c0ea678437068a411053ed8ea7a68aab
unknown <alexey.khomiak@gmail.com>
2018-08-07 13:01:45 +0300

    TaskManager

---
+++ TaskManager2/TaskManager2/RelayCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TaskManager2
+{
+    class RelayCommand : ICommand
+    {
+        readonly Action<object> _act;
+        readonly Predicate<object> _pred;
+        public RelayCommand(Action<object> act, Predicate<object> pred = null)
+        {
+            _act = act;
+            _pred = pred;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _pred == null ? true : _pred(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _act(parameter);
+        }
+    }
+}
---
+++ TaskManager2/TaskManager2/ViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace TaskManager2
+{
+    public class ViewModel
+    {
+        public ObservableCollection<Process> Processes { get; set; }
+        public Process CurrentProcess { get; set; }
+        public List<int> currentIds;
+
+        public ViewModel()
+        {
+            Processes = new ObservableCollection<Process>();
+            
+            currentIds = Processes.Select(x => x.Id).ToList();
+
+            foreach (var i in Process.GetProcesses())
+            {
+                Processes.Add(i);
+            }
+
+            StopProcess = new RelayCommand(x =>
+            {
+                CurrentProcess.CloseMainWindow();
+                Processes.Remove(CurrentProcess);
+                foreach (var i in Process.GetProcesses())
+                    {
+                        Processes.Add(i);
+                    }
+            });
+
+            RefreshProcess = new RelayCommand(x =>
+            {
+                foreach (var i in Process.GetProcesses())
+                {
+                    Processes.Add(i);
+                }
+            });
+
+        }
+
+        public ICommand StopProcess { get; set; }
+        public ICommand RefreshProcess { get; set; }
+
+
+    }
+}
